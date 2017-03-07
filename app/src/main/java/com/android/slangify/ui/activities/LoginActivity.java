6d2b6f4a6b9223adc2ad60f5d0d90f3d29d77540
bettykin <bettykin@gmail.com>
2017-03-07 21:01:48 +0200

    Added LoginActivity

---
+++ app/src/main/java/com/android/slangify/ui/activities/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.android.slangify.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android.slangify.R;
+
+/**
+ * Created by bettykin on 07/03/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
