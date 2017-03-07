ac555675a4212e69063db6e3f11143ac4798c07d
Limor Mekaiten <limor.mekaiten@petpace.com>
2017-03-07 08:54:02 +0200

    Updated app icon

---
+++ app/src/main/java/com/android/slangify/CaptureVideoActivity.java
@@ -0,0 +1,19 @@
+package com.android.slangify;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by limormekaiten on 3/6/17.
+ */
+
+public class CaptureVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_capture_video);
+    }
+}
