f0735524c1422dccca34018d4bb2053f4b7aaf3d
Limor Mekaiten <limor.mekaiten@petpace.com>
2017-03-10 12:10:05 +0200

    video display midwork

--- app/src/main/java/com/android/slangify/ui/activities/DisplayVideoActivity.java
+++ app/src/main/java/com/android/slangify/ui/activities/DisplayVideoActivity.java
@@ -1,14 +1,22 @@
 package com.android.slangify.ui.activities;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.android.slangify.R;
 import com.android.slangify.dialog.FancyAlertDialog;
 import com.devspark.robototextview.widget.RobotoTextView;
+import com.volokh.danylo.video_player_manager.manager.PlayerItemChangeListener;
+import com.volokh.danylo.video_player_manager.manager.SingleVideoPlayerManager;
+import com.volokh.danylo.video_player_manager.manager.VideoPlayerManager;
+import com.volokh.danylo.video_player_manager.meta.MetaData;
+import com.volokh.danylo.video_player_manager.ui.SimpleMainThreadMediaPlayerListener;
+import com.volokh.danylo.video_player_manager.ui.VideoPlayerView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,6 +29,8 @@ public class DisplayVideoActivity extends AppCompatActivity {
 
     @BindView(R.id.dialog_btn)
     TextView btnSubmit;
+    @BindView(R.id.video)
+    VideoView video;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,5 +47,9 @@ public class DisplayVideoActivity extends AppCompatActivity {
                 fancyAlertDialog.show();
             }
         });
+
+        video.setVideoURI(Uri.parse("https://www.youtube.com/watch?v=MaHq0Gh6e2k"));
+
+        video.start();
     }
 }
