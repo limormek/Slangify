d905fbbfdfec9be0cd8a70fa6e83fff01c17fd46
Limor Mekaiten <limor.mekaiten@petpace.com>
2017-03-10 09:53:09 +0200

    display video activity

--- app/src/main/java/com/android/slangify/ui/activities/DisplayVideoActivity.java
+++ app/src/main/java/com/android/slangify/ui/activities/DisplayVideoActivity.java
@@ -1,10 +1,21 @@
 package com.android.slangify.ui.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.android.slangify.R;
 
 /**
  * Created by limormekaiten on 3/8/17.
  */
 
 public class DisplayVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_display_video);
+    }
 }
