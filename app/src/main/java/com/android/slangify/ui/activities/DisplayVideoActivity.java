9c28a25400cface6a391528f75da5c1f4556cd8a
Limor Mekaiten <limor.mekaiten@petpace.com>
2017-03-10 12:33:24 +0200

    fixed errors

--- app/src/main/java/com/android/slangify/ui/activities/DisplayVideoActivity.java
+++ app/src/main/java/com/android/slangify/ui/activities/DisplayVideoActivity.java
@@ -10,13 +10,6 @@ import android.widget.VideoView;
 
 import com.android.slangify.R;
 import com.android.slangify.dialog.FancyAlertDialog;
-import com.devspark.robototextview.widget.RobotoTextView;
-import com.volokh.danylo.video_player_manager.manager.PlayerItemChangeListener;
-import com.volokh.danylo.video_player_manager.manager.SingleVideoPlayerManager;
-import com.volokh.danylo.video_player_manager.manager.VideoPlayerManager;
-import com.volokh.danylo.video_player_manager.meta.MetaData;
-import com.volokh.danylo.video_player_manager.ui.SimpleMainThreadMediaPlayerListener;
-import com.volokh.danylo.video_player_manager.ui.VideoPlayerView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
